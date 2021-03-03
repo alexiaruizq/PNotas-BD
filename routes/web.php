3a595b3758e321d7505f037c1b652ace42498298
Alexia Ruiz Quiroz <alexiaruiz@MacBook-Pro-de-Alexia.local>
2021-03-03 12:02:09 -0700

    pnotas

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+use App\Models\Notas;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('notas', function(){
+    $notas = Notas::all();//DB::table('notas')->get();
+
+    
+
+    return view('notas', ['notas'=> $notas]);
+
+})->name('notas.index');
+
+
+
+Route::get('agregar', function(){
+    return view('agregar');
+
+});
+
+Route::get('notas/{id}/editar', function ($id){
+    $notas = Notas::find($id); //DB::table('notas')->where('id',$id)->first();
+
+    return view('editar', ['notas'=> $notas]);
+})->name('notas.edit');
